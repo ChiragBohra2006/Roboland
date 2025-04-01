dfff4a83ddfd64199c91208276668565862e8bda
ChiragBohra2006 <76206854+ChiragBohra2006@users.noreply.github.com>
2025-04-01 20:37:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -134,7 +134,7 @@ function preload(){
 
     healthBarImage = loadImage("hb.png");
   
-    batteryBarImage = loadImage("BatteryBar.png");
+    batteryBarImage = loadImage("batteryBar.png");
 
     rhealthImage = loadImage("health.png");
 
